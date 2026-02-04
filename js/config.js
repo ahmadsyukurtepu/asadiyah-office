a918f7a4c508809f7f36396024464f486a60c8ba
ahmadsyukurtepu <ahmadsyukurtepu@gmail.com>
2026-02-05 01:08:34 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
-// js/config.js
+// Konfigurasi Database hasil bedah file index(13)
 const CONFIG = {
-    API_KEY: 'AIzaSyCJ3Nhpao_DBfgQyDxH-0QZ1GHfiHRQVmo',
-    DB_LOGIN: '1HTdBcYKE1ztWtj1u_6rtC0xAmE-Mknl_4mEG6og1xPg',
-    DB_SURAT: '1BrvgSrI_4PdTZx95jbGY_H21bSHZpl0fduNEfPRZlr0',
-    DB_SANTRI_PUSAT: '1E_940vnYcGQAgyB2mLmUFtdSmKCJ-ZXmqv7q4-Dr6yI',
-    DB_SANTRI_CABANG: '1SD5AYyDYBUU892NoVZiyr6SS-j2XkDHzHSWkfyuf7eA',
-    DB_PEGAWAI: '1LUnBymrWCxc5uWVXH3LGUhedrbQHaq1dvTRDsY707fg',
-    DB_AGENDA: '1YOJEBteDe_DYeLq31hw1VUPtxfV8rDdlpZ-XsBjMFYk',
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwUR0VPkWF0wQhtAGcs1-OuT3YYC81-izAydqEEc3VvIEneO5AezIkl2V8o09BKbYHC/exec'
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwUR0VPkWF0wQhtAGcs1-OuT3YYC81-izAydqEEc3VvIEneO5AezIkl2V8o09BKbYHC/exec',
+    DB: {
+        LOGIN: '1HTdBcYKE1ztWtj1u_6rtC0xAmE-Mknl_4mEG6og1xPg',
+        SURAT: '1BrvgSrI_4PdTZx95jbGY_H21bSHZpl0fduNEfPRZlr0',
+        SANTRI_PUSAT: '1E_940vnYcGQAgyB2mLmUFtdSmKCJ-ZXmqv7q4-Dr6yI',
+        SANTRI_CABANG: '1SD5AYyDYBUU892NoVZiyr6SS-j2XkDHzHSWkfyuf7eA',
+        PEGAWAI: '1LUnBymrWCxc5uWVXH3LGUhedrbQHaq1dvTRDsY707fg',
+        AGENDA: '1YOJEBteDe_DYeLq31hw1VUPtxfV8rDdlpZ-XsBjMFYk'
+    }
 };
