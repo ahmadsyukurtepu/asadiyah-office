cb13b39bbd864e1820d29bf0292492940867a977
ahmadsyukurtepu <ahmadsyukurtepu@gmail.com>
2026-02-05 07:43:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,30 @@
-// Konfigurasi Database hasil bedah file index(13)
+/**
+ * CONFIGURATION SYSTEM - KANTOR PUSAT AS'ADIYAH
+ * Update Terakhir: 2026-02-05
+ */
+
 const CONFIG = {
+    // URL Google Apps Script (Web App URL)
     SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbywFZkPsNg0d53Lft2KVeYF89Z3yZSYsn9mtK0_qb_aBG8Bwn3MhRSa9YwVnjnQCaXbFg/exec',
+    
+    // ID Spreadsheet (Pastikan izin akses: Anyone with the link as Viewer/Editor)
     DB: {
         LOGIN: '1HTdBcYKE1ztWtj1u_6rtC0xAmE-Mknl_4mEG6og1xPg',
+        
+        // Modul Persuratan
         SURAT: '1BrvgSrI_4PdTZx95jbGY_H21bSHZpl0fduNEfPRZlr0',
+        
+        // Modul Data Santri (Pusat & Cabang Terpisah)
         SANTRI_PUSAT: '1E_940vnYcGQAgyB2mLmUFtdSmKCJ-ZXmqv7q4-Dr6yI',
         SANTRI_CABANG: '1SD5AYyDYBUU892NoVZiyr6SS-j2XkDHzHSWkfyuf7eA',
+        
+        // Modul Kepegawaian / SDM
         PEGAWAI: '1LUnBymrWCxc5uWVXH3LGUhedrbQHaq1dvTRDsY707fg',
+        
+        // Modul Agenda & Disposisi
         AGENDA: '1YOJEBteDe_DYeLq31hw1VUPtxfV8rDdlpZ-XsBjMFYk'
     }
 };
+
+// Mencegah perubahan objek secara tidak sengaja di browser console
+Object.freeze(CONFIG);
